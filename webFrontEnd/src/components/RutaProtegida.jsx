1b4048ebb43716f1d28b5dd9a241314bdced7c3a
davidch8931 <david.chasi8931@utc.edu.ec>
2026-01-21 09:58:28 -0500

    arreglo recoger token

--- webFrontEnd/src/components/RutaProtegida.jsx
+++ webFrontEnd/src/components/RutaProtegida.jsx
@@ -3,12 +3,12 @@ import { Navigate, Outlet } from "react-router-dom";
 const RutaProtegida = () => {
     const token = localStorage.getItem("access_token");
 
-    // Si NO hay token, lo mandamos al Login (replace para que no pueda volver)
+    // Si NO hay token lo mandamos al Login
     if (!token) {
         return <Navigate to="/" replace />;
     }
 
-    // Si SÍ hay token, dejamos pasar a las rutas hijas (Outlet)
+    // Si hay token, dejamos pasar a las rutas hijas
     return <Outlet />;
 };
 
